ee8ac4a595d591dbbe89c72abb58b04c1b949551
Christopher Weiss <cmweiss@gmail.com>
2015-02-10 10:58:24 -0500

    Update template.js

--- templateHTML/template.js
+++ templateHTML/template.js
@@ -1,33 +1,44 @@
 function loadTemplate() {
-	var body = document.getElementsByTagName("body")[0];
-	var midsec = document.createElement("div");
-	var content = document.createElement("div");
-	var header = document.createElement("div");
-	var footer = document.createElement("div");
-	var leftSide = document.createElement("div");
-	var rightSide = document.createElement("div")
+	'use strict';
+	var body = document.body,
+		midsec = document.createElement('div'),
+		content = document.createElement('div'),
+		header = document.createElement('div'),
+		footer = document.createElement('div'),
+		leftSide = document.createElement('div'),
+		rightSide = document.createElement('div'),
 		bodyContents = document.createRange();
-	
-	header.setAttribute("id","header");
-	midsec.setAttribute("id","midsection");
-	content.setAttribute("id","content");
-	footer.setAttribute("id","footer");
-	leftSide.setAttribute("id","leftside");
-	rightSide.setAttribute("id","rightside");
+
+	function sectionFetcher(url, destinationParent) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url);
+		xhr.onreadystatechange = function () {
+			if(xhr.readyState === 4) {
+				document.getElementById(destinationParent).innerHTML = xhr.responseText;
+			}
+		}
+	}
+
+	header.setAttribute('id', 'header');
+	midsec.setAttribute('id', 'midsection');
+	content.setAttribute('id', 'content');
+	footer.setAttribute('id', 'footer');
+	leftSide.setAttribute('id', 'leftside');
+	rightSide.setAttribute('id', 'rightside');
 	
 	bodyContents.setStart(body.firstChild, 0);
 	bodyContents.setEndBefore(body.getElementsByTagName('script')[0]);
 	
 	content.appendChild(bodyContents.extractContents());
 	body.appendChild(content);
-	body.insertBefore(midsec,content);
-	body.insertBefore(header,midsec);
+	body.insertBefore(midsec, content);
+	body.insertBefore(header, midsec);
 	midsec.appendChild(leftSide);
 	midsec.appendChild(content);
 	midsec.appendChild(rightSide);
 	body.appendChild(footer);
-	ajaxFunction("header.htm","header");
-	ajaxFunction("leftnav.htm","leftside");
-	ajaxFunction("rightnav.htm","rightside");
-	ajaxFunction("footer.htm","footer");
+	sectionFetcher('header.htm', 'header');
+	sectionFetcher('leftnav.htm', 'leftside');
+	sectionFetcher('rightnav.htm', 'rightside');
+	sectionFetcher('footer.htm', 'footer');
 }
